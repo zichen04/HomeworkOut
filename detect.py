eb77403576c6e3ae4c53dd9995618524a6d23e3e
zichen04 <90058879+zichen04@users.noreply.github.com>
2024-10-02 18:09:42 -0700

    PUSH UP DETECTION FUNCTION WORKS!!!

--- detect.py
+++ detect.py
@@ -2,8 +2,8 @@ import cv2
 import cv2.legacy
 
 def detectPushup() -> int:
+    state = "up"
     nums = 0
-    pushup = 0
     isFirst = True
     initialYPos = 0
 
@@ -17,19 +17,18 @@ def detectPushup() -> int:
 
     ret, frame = webcamIn.read()
 
-    # create a bbox and intialize a KCF tracker object
+    # create a bounding box and intialize a KCF tracker object
     bbox = (180, 250, 450, 200)
-    boxCenter = (int(bbox[0] + 0.5*bbox[2]), int(bbox[1] + 0.5*bbox[3]))
-    print(f"x: {bbox[0]}, y: {bbox[1]}, width: {bbox[2]}, height: {bbox[3]}")
+    boxCenter = (int(bbox[0] + 0.5*bbox[2]), int(bbox[1] + 0.5*bbox[3]))    # center of the bounding box
 
     trackPerson = cv2.legacy.TrackerKCF.create()
     
     trackPerson.init(frame, bbox)
     
-    # give the user 15 seconds to get into position
-    for i in range(15):
+    # give the user 10 seconds to get into position
+    for i in range(10):
         frame[:] = 0
-        cv2.putText(frame, str(15 - i), (320, 240), cv2.FONT_HERSHEY_DUPLEX, 0.75, (0, 0, 255), 2)
+        cv2.putText(frame, str(10 - i), (320, 240), cv2.FONT_HERSHEY_DUPLEX, 0.75, (0, 0, 255), 2)
         cv2.imshow("Tracking", frame)
         cv2.waitKey(1000)
 
@@ -37,46 +36,49 @@ def detectPushup() -> int:
 
         ret, frame = webcamIn.read()
 
-        # Update the tracker with the new frame
+        # update the tracker with new frame
         success, bbox = trackPerson.update(frame)
+
+
+        if nums == 10:
+            break
+
 
         if success:
             # draw the bounding box
             x, y, w, h = [int(v) for v in bbox]
             boxCenter = (int(x + 0.5*w), int(y + 0.5*h))
 
+            # if its the very first frame, take the initial center position of that and store it
             if isFirst:
                 initialYPos = boxCenter[1]
                 isFirst = False
-            else:
-                if boxCenter[1] > initialYPos + 27: 
-                    pushup = 1
-                elif boxCenter[1] < initialYPos + 5:
-                    nums += pushup
-                    pushup = 0
 
-            cv2.putText(frame, str(boxCenter[1]), (20, 50), cv2.FONT_HERSHEY_DUPLEX, 0.75, (0, 0, 255), 2)
-            cv2.putText(frame, str(nums), (0, 100), cv2.FONT_HERSHEY_DUPLEX, 0.75, (0, 0, 255), 2)
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)  # Green rectangle
-            cv2.circle(frame, boxCenter, 2, (0, 0, 255), 2)  # point
+            # use initial center position to count pushups
+            else:                                                                   
+                if state == "up" and boxCenter[1] > initialYPos + 25:
+                    state = "down"
+                elif state == "down" and boxCenter[1] < initialYPos + 7:
+                    state = "up"
+                    nums += 1
+
+            cv2.putText(frame, "pushups count: " + str(nums), (20, 100), cv2.FONT_HERSHEY_DUPLEX, 0.75, (0, 0, 255), 2) # number of pushups done
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)  # rectangle around object being tracked
+            cv2.circle(frame, boxCenter, 2, (0, 0, 255), 2)  # point at center
 
             boxCenter = (x + 0.5*w, y + 0.5*h)
 
-        else: 
-            # If tracking fails, display a message
-            cv2.putText(frame, "Tracking failed", (350, 350), cv2.FONT_HERSHEY_DUPLEX, 0.75, (0, 0, 255), 2)
-
-        # Display the frame with the tracking result
+        # display the frame
         cv2.imshow("Tracking", frame)
 
             
-        #escape from program
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+        #escape from program with E
+        if cv2.waitKey(1) & 0xFF == ord('e'):
             break
 
     webcamIn.release()
     cv2.destroyAllWindows()
     
-    return 10
+    return nums
 
 
