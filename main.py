4bde5393bbf5e89c71917bb0de6fb7d3f258525d
zichen04 <90058879+zichen04@users.noreply.github.com>
2024-10-04 21:07:33 -0700

    blocking input and killing chrome

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 import detect
 import youtube
+import ctypes
+import os
+
+# Block input (True = block, False = unblock)
+def blockInputs(block):
+    ctypes.windll.user32.BlockInput(block)
+
+def killChrome():
+    try:
+        os.system('tskill chrome')
+    except:
+        print("Chrome is already closed")
+
 
 def main():
     minutes = 0
@@ -7,7 +20,11 @@ def main():
     while True:     
         
         if (youtube.isTimeUp()):
+            blockInputs(True)
+
             if (detect.detectPushup()):
-                # do stuff
                 print("good job!")
-    
+                blockInputs(False)
+
+            else:
+                killChrome()
